7260972c8ddea62793efebedba332ac41a02d09d
Dailion J <DJanga@hoppenbrouwers.nl>
2025-11-14 11:26:47 +0100

    with plugin

---
+++ stack/configuration/plugins.py
@@ -0,0 +1,11 @@
+# Add your plugins and plugin settings here.
+
+# Topology Views plugin
+PLUGINS = ["netbox_topology_views"]
+
+PLUGINS_CONFIG = {
+    "netbox_topology_views": {
+        # You can leave this empty for now – defaults are fine
+        # Example options exist in the plugin docs if you want to tweak behaviour
+    }
+}
